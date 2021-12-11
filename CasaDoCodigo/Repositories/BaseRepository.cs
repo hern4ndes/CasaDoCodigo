58689ad86b1e1f555df17defc83935800802de6a
Hernandes <hernandesrodrigues@pecege.com>
2021-12-11 20:14:50 -0300

    adicionando arquivos

---
+++ CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo.Repositories
+{
+   
+    public class BaseRepository<T> where  T : BaseModel
+    {
+        protected readonly ApplicationContext contexto;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepository(ApplicationContext context)
+        {
+            this.contexto = context;
+            dbSet =  context.Set<T>();
+        }
+    }
+}
