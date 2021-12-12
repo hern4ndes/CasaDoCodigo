ccd673b2bf7c7d9202d93c1f68d1f3a2cb3c8033
Hernandes <hernandesrodrigues@pecege.com>
2021-12-11 21:15:48 -0300

    Implementado modificação de quantidade

--- CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -1,15 +1,32 @@
 ﻿using CasaDoCodigo.Models;
+using System.Linq;
 
 namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
+        void UpdateQuantidade(ItemPedido itemPedido);
 
     }
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext context) : base(context)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDB = dbSet
+                                .Where(ip => ip.Id == itemPedido.Id)
+                                .SingleOrDefault();
+
+            if (itemPedidoDB != null)
+            {
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
+                contexto.SaveChanges();
+            }
+
+
         }
     }
 }
